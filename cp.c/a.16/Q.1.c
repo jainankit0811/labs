65013f2956e3cfb9e852a1b467056c61fb9cca50
jainankit0811 <ankitd0811@gmail.com>
2022-11-09 14:29:55 +0530

    update

--- cp.c/a.16/Q.1.c
+++ cp.c/a.16/Q.1.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 
 int main()
@@ -12,19 +11,5 @@ int main()
         printf("%d\n",s);
     }
     return 0;
-=======
-#include <stdio.h>
 
-int main()
-{
-    int n,i,s;
-    printf("Enter number.\n");
-    scanf("%d",&n);
-    for (i=1; i<=n; i++)
-    {
-        s=s+i;
-        printf("%d\n",s);
-    }
-    return 0;
->>>>>>> 236851ad9c8c6be1eb09fb4426bf9c8dd248b1ad
 }
