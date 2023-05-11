6cf3c724b6842eca050b084a12956934c668b84a
jainankit0811 <ankitd0811@gmail.com>
2023-05-12 03:00:23 +0530

    s

--- book Quction/A-normal_program/digit_in_reverse.c
+++ book Quction/A-normal_program/digit_in_reverse.c
@@ -3,20 +3,5 @@
 
 int main()
 {
-    int n = 12345, a, b = 10, c = 5;
-
-    printf("Number is-%d\n", n);
-
-    while (c != 0)
-    {
-        a = n % b;
-
-        printf("%d\n", a);
-
-        n = n / b;
-
-        c = c - 1;
-    }
-
-    return 0;
+    string return 0;
 }
