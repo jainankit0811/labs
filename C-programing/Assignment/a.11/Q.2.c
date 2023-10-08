8f7f13df28eaa83cbcd2e43af1b750c585df921d
jainankit0811 <ankitd0811@gmail.com>
2023-10-08 18:23:25 +0530

    update

--- C-programing/Assignment/a.11/Q.2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-// Observe the utput of every statement
-int main()
-{
-	int a = 0;
-	double d = 10.21;
-	printf("%lu\n", sizeof(a + d));
-	return 0;
-}
--- C-programing/Assignment/a.9/Q.8.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int a,b;
-    printf("Enter number\n");
-        scanf ("%d%d",&a,&b);
-        printf("After swap %d%d\n",b,a);
-        return 0;
-}
